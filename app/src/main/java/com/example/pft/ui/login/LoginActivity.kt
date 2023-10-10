9bab018e4d2a3d63f106c070dd62524e84fc48ab
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2023-10-09 22:06:46 -0300

    inicio sprint 3

---
+++ app/src/main/java/com/example/pft/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pft.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pft.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/pft/ui/login/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pft.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pft.R
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
