4f837b1ecb2935660f1caab15c9f8a57829f3ad2
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2023-10-29 18:21:27 -0300

    retoques navegación analista, tiene problema de id duplicado nav_host_fragment, por eso tiene comentada la creación del fragmento en content_main_analista.xml

--- app/src/main/java/com/example/pft/MainActivity_analista.kt
+++ app/src/main/java/com/example/pft/MainActivity_analista.kt
@@ -9,21 +9,31 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.pft.databinding.ActivityMainAnalistaBinding
 import com.example.pft.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
+import java.util.Locale
 
 private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityMainAnalistaBinding
 class MainActivity_analista : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_analista)
-        /*
-        setSupportActionBar(binding.appBarMain.toolbar)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main_analista)
+        val locale = Locale("es", "ES")
+        Locale.setDefault(locale)
+
+        binding = ActivityMainAnalistaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMainAnalista.toolbarAnalista)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayoutAnalista
+
+       val navView: NavigationView = binding.navViewAnalista
+/*
+        val navController = findNavController(R.id.nav_host_fragment_analista)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -31,20 +41,20 @@ class MainActivity_analista : AppCompatActivity() {
                 R.id.nav_perfil_analista, R.id.nav_eventos_analista, R.id.nav_reclamos_analista
             ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+       setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main_analista)
+        val navController = findNavController(R.id.nav_host_fragment_analista)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-    */
+
+     */
     }
 }
+
+
+
