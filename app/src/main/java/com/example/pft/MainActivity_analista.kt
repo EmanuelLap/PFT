4654bb5a6b666976344356f93d5666c427bebda5
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2023-11-17 19:28:03 -0300

    avances en Analista Reclamo y analista main

--- app/src/main/java/com/example/pft/MainActivity_analista.kt
+++ app/src/main/java/com/example/pft/MainActivity_analista.kt
@@ -3,7 +3,11 @@ package com.example.pft
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
 import com.example.pft.ui.eventos.Evento_analistaActivity
 import com.example.pft.ui.login.LoginActivity
 import com.example.pft.ui.perfil.PerfilActivity
@@ -15,7 +19,7 @@ import java.util.Locale
 
 class MainActivity_analista : AppCompatActivity() {
 
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var icon: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +28,26 @@ class MainActivity_analista : AppCompatActivity() {
         val locale = Locale("es", "ES")
         Locale.setDefault(locale)
 
+        val toolbar: Toolbar = findViewById(R.id.analista_toolbar)
+        setSupportActionBar(toolbar)
 
-        val perfilActivity = Intent(this, PerfilActivity::class.java)
-        val eventosActivity = Intent(this, Evento_analistaActivity::class.java)
-        val reclamosActivity = Intent(this, Reclamo_analistaActivity::class.java)
-        val usuariosActivity = Intent(this, Usuarios_AnalistaActivity::class.java)
-        val loginActivity = Intent(this, LoginActivity::class.java)
+        // Encuentra el ícono en el menú inflado
+        val menu = toolbar.menu
+        val menuItem = menu.findItem(R.id.toolbar_icon)
+        // Encuentra el ícono en la vista de acción de la barra de herramientas
+        val ab = supportActionBar
+        ab?.setDisplayShowCustomEnabled(true)
+        ab?.setCustomView(R.layout.app_bar_main_analista) // Reemplaza con tu layout
 
+        // Encuentra el ImageView en el layout personalizado de la barra de herramientas
+        icon = ab?.customView?.findViewById(R.id.toolbar_icon) as ImageView
+
+        icon.setOnClickListener {
+            showPopupMenu(it)
+        }
+
+
+/*
         bottomNavigationView = findViewById(R.id.analistaBottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -43,33 +60,39 @@ class MainActivity_analista : AppCompatActivity() {
             true
         }
 
-        /*
-        // Puedes agregar un ícono y hacer clic en él para mostrar el menú emergente
-        val moreOptionsIcon = bottomNavigationView.findViewById<View>(R.id.menuAnalistaMasOpciones)
-        moreOptionsIcon.setOnClickListener {
-            showPopupMenu(moreOptionsIcon)
-        }
+ */
+
     }
 
     private fun showPopupMenu(view: View) {
         val popupMenu = PopupMenu(this, view)
-        popupMenu.menuInflater.inflate(R.menu.menu_mas_opciones_analista, popupMenu.menu)
-        popupMenu.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.menuAnalistaReclamo -> {
-                    // Abre la actividad de reclamos u realiza la acción deseada
-                    true
-                }
-                // Agrega más opciones según sea necesario
-                else -> false
-            }
-        }
+        popupMenu.inflate(R.menu.main_analista_menu) // Infla el menú correcto
         popupMenu.show()
 
+        val perfilActivity = Intent(this, PerfilActivity::class.java)
+        val eventosActivity = Intent(this, Evento_analistaActivity::class.java)
+        val reclamosActivity = Intent(this, Reclamo_analistaActivity::class.java)
+        val usuariosActivity = Intent(this, Usuarios_AnalistaActivity::class.java)
+        val loginActivity = Intent(this, LoginActivity::class.java)
 
-         */
+
+        // Maneja los clics en los elementos del menú aquí
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menuAnalistaPerfil -> startActivity(perfilActivity)
+                R.id.menuAnalistaUsuarios -> startActivity(usuariosActivity)
+                R.id.menuAnalistaEventos -> startActivity(eventosActivity)
+                R.id.menuAnalistaReclamos -> startActivity(reclamosActivity)
+                R.id.menuAnalistaCerrarSesión -> startActivity(loginActivity)
+            }
+            true
+        }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_analista_menu, menu)
+        return true
+    }
 
 }
 
--- app/src/main/java/com/example/pft/ui/reclamos/Reclamo_analistaActivity.kt
+++ app/src/main/java/com/example/pft/ui/reclamos/Reclamo_analistaActivity.kt
@@ -1,12 +1,105 @@
 package com.example.pft.ui.reclamos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.pft.ApiService
+import com.example.pft.MainActivity_analista
 import com.example.pft.R
+import com.example.pft.entidades.Evento
+import com.example.pft.entidades.Reclamo
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Reclamo_analistaActivity : AppCompatActivity() {
+
+    private lateinit var usuario: Spinner
+    private lateinit var estado: Spinner
+    private lateinit var listaReclamos: ListView
+    private lateinit var volver: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reclamo_analista)
+
+        usuario=findViewById(R.id.reclamoAnalistaActivity_usuario)
+        estado=findViewById(R.id.reclamoAnalista_estado)
+        listaReclamos=findViewById(R.id.reclamoAnalista_lista)
+        volver=findViewById(R.id.reclamoAnalista_volver)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val call=apiService.obtenerReclamos()
+        Log.d("ReclamoAnalistaActivity", "Before API call")
+
+        call.enqueue(object : Callback<List<Reclamo>> {
+            override fun onResponse(call: Call<List<Reclamo>>, response: Response<List<Reclamo>>) {
+                if (response.isSuccessful) {
+                    val reclamos = response.body()
+                    Log.d("ReclamoAnalistaActivity", "API call successful. Reclamos: $reclamos")
+
+                    // Configurar el ArrayAdapter
+                    val adapter = ArrayAdapter(
+                        this@Reclamo_analistaActivity,
+                        android.R.layout.simple_list_item_1,
+                        reclamos?.map { "${it.titulo}\n${it.estudianteId.nombres} ${it.estudianteId.apellidos}\nSemestre ${it.semestre}"  } ?: emptyList()
+                    )
+
+                    // Asignar el adapter al ListView
+                    listaReclamos.adapter = adapter
+                    /*
+                                        // Al realizar click en cualquier elemento de la lista
+                                        listaEventos.setOnItemClickListener { adapterView, view, i, l ->
+                                            val eventoSeleccionado = eventos!!.get(i)
+
+                                            // Convierte el objeto Evento a una cadena JSON (por ejemplo, utilizando Gson)
+                                            val eventoJson = Gson().toJson(eventoSeleccionado)
+
+                                            // Crea un Intent y agrega la cadena JSON como extra
+                                            evento.putExtra("evento", eventoJson)
+
+                                            // Iniciar la actividad con el Intent configurado
+                                            startActivity(evento)
+                                        }
+
+                     */
+                }
+
+
+                else {
+
+                    Log.e("ReclamoAnalistaActivity", "API call failed with code ${response.code()}")
+                    // Resto del código para manejar errores...
+                }
+            }
+
+            override fun onFailure(call: Call<List<Reclamo>>, t: Throwable) {
+                Log.e("ReclamoAnalistaActivity", "API call failed", t)
+                // Resto del código para manejar errores...
+            }
+        })
+
+        volver.setOnClickListener{
+            val mainActivity = Intent(this, MainActivity_analista::class.java)
+            startActivity(mainActivity)
+        }
+
     }
+
+
 }
