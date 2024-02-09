2e2aa44219445571923791341c1bc4cc27f4d4fb
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2024-02-09 14:09:21 -0300

    Primeros cambios Perfil

--- app/src/main/java/com/example/pft/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/example/pft/ui/perfil/PerfilFragment.kt
@@ -5,56 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.pft.R
+import com.example.pft.UsuarioSingleton
+import com.example.pft.entidades.EstudianteId
+import com.google.gson.Gson
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PerfilFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PerfilFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var nombre: TextView
+    private lateinit var imagen_perfil: ImageView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_perfil, container, false)
+
+        nombre=view.findViewById(R.id.perfilFragment_nombreApellido)
+        imagen_perfil=view.findViewById(R.id.perfilFragment_imagen)
+
+        // Recuperar el valor del "usuario"
+
+        val usuarioJson = UsuarioSingleton.usuario
+
+        // Convertir la cadena JSON de vuelta a un objeto Usuario (usando Gson)
+        val usuario = Gson().fromJson(usuarioJson, EstudianteId::class.java)
+
+        // Determinar qué imagen de perfil cargar según el género del usuario
+        val imagenPerfil = if (usuario.genero == "F") {
+            R.drawable.drawable_perfil_femenino
+        } else {
+            R.drawable.drawable_perfil_masculino
+        }
+
+        // Establecer la imagen de perfil
+        imagen_perfil.setImageResource(imagenPerfil)
+        nombre.text=usuario.nombres + "\n" + usuario.apellidos
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PerfilFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PerfilFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
