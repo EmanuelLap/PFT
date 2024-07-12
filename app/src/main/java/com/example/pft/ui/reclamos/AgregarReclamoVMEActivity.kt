a6414f0f560d1a14c6579d1fe88c66245d5bbcea
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2024-07-12 14:18:15 -0300

    Reclamo VME verificaciones (no cumple con los requisitos)

--- app/src/main/java/com/example/pft/ui/reclamos/AgregarReclamoVMEActivity.kt
+++ app/src/main/java/com/example/pft/ui/reclamos/AgregarReclamoVMEActivity.kt
@@ -1,8 +1,10 @@
 package com.example.pft.ui.reclamos
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +13,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.example.pft.ApiService
 import com.example.pft.R
+import com.example.pft.UsuarioSingleton
+import com.example.pft.entidades.ReclamoDTOMobile
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
@@ -31,6 +43,13 @@ class AgregarReclamoVMEActivity : AppCompatActivity() {
     private lateinit var docente: Spinner
     private lateinit var agregarReclamo: FloatingActionButton
     private lateinit var volver: FloatingActionButton
+
+    //mensajes
+    private lateinit var mensaje_titulo: TextView
+    private lateinit var mensaje_detalle: TextView
+    private lateinit var mensaje_empresa: TextView
+    private lateinit var mensaje_creditos: TextView
+    private lateinit var mensaje_fecha: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_reclamo_vme)
@@ -41,7 +60,6 @@ class AgregarReclamoVMEActivity : AppCompatActivity() {
         //Declaro rutas a elementos
         titulo=findViewById(R.id.agregarReclamoVME_Titulo)
         descripcion=findViewById(R.id.agregarReclamoVME_Descripcion)
-        nombre_actividad=findViewById(R.id.agregarReclamoVME_VME)
         creditos=findViewById(R.id.agregarReclamoVME_creditos)
         fechaButton = findViewById(R.id.agregarReclamoVME_fecha)
         fechaText=findViewById(R.id.agregarReclamoVME_fecha_seleccionada)
@@ -50,6 +68,17 @@ class AgregarReclamoVMEActivity : AppCompatActivity() {
         empresa=findViewById(R.id.agregarReclamoVME_VME)
         docente=findViewById(R.id.agregarReclamoVME_docente)
         volver=findViewById(R.id.agregarReclamoVME_volver)
+
+        //mensajes
+        mensaje_titulo=findViewById(R.id.agregarReclamoVMEActivity_mensaje_titulo)
+        mensaje_detalle=findViewById(R.id.agregarReclamoVMEActivity_mensaje_descripcion)
+        mensaje_creditos=findViewById(R.id.agregarReclamoVMEActivity_mensaje_creditos)
+        mensaje_empresa=findViewById(R.id.agregarReclamoVMEActivity_mensaje_vme)
+        mensaje_fecha=findViewById(R.id.agregarReclamoVMEActivity_mensaje_fecha)
+
+        // Recuperar el valor del "usuario"
+
+        val usuario = UsuarioSingleton.usuario
 
         //spinner semestre
         // opciones (del 1 al 10)
@@ -94,6 +123,121 @@ class AgregarReclamoVMEActivity : AppCompatActivity() {
             mostrarCalendario()
         }
 
+        agregarReclamo.setOnClickListener {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")  // Reemplaza "tu_direccion_ip" con la dirección IP de tu máquina de desarrollo
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val apiService = retrofit.create(ApiService::class.java)
+
+            val camposVacios = mutableListOf<String>()
+
+            if (titulo.text.toString().isEmpty()) {
+                camposVacios.add("titulo")
+                mensaje_titulo.text = "Ingresa un titulo"
+                mensaje_titulo.alpha = 0.8f
+                mensaje_titulo.visibility = View.VISIBLE
+            } else {
+                mensaje_titulo.visibility = View.INVISIBLE
+            }
+
+            if (descripcion.text.toString().isEmpty()) {
+                camposVacios.add("descripcion")
+                mensaje_detalle.text = "Ingresa una descripción"
+                mensaje_detalle.alpha = 0.8f
+                mensaje_detalle.visibility = View.VISIBLE
+            } else {
+                mensaje_detalle.visibility = View.INVISIBLE
+            }
+
+            if (fechaText.text.toString().isEmpty()) {
+                camposVacios.add("Fecha")
+                mensaje_fecha.text = "Selecciona una fecha"
+                mensaje_fecha.alpha = 0.8f
+                mensaje_fecha.visibility = View.VISIBLE
+            } else {
+                mensaje_fecha.visibility = View.INVISIBLE
+            }
+            if (empresa.text.toString().isEmpty()) {
+                camposVacios.add("actividad")
+                mensaje_empresa.text = "Selecciona una actividad"
+                mensaje_empresa.alpha = 0.8f
+                mensaje_empresa.visibility = View.VISIBLE
+            } else {
+                mensaje_empresa.visibility = View.INVISIBLE
+            }
+            if (creditos.text.toString().isEmpty()) {
+                camposVacios.add("creditos")
+                mensaje_creditos.text = "Ingresa la cantidad de créditos"
+                mensaje_creditos.alpha = 0.8f
+                mensaje_creditos.visibility = View.VISIBLE
+            } else {
+                mensaje_creditos.visibility = View.INVISIBLE
+            }
+
+            if (camposVacios.isNotEmpty()) {
+                Toast.makeText(this, "Por favor, completa todos los campos.", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // Todos los campos están completos
+
+                val formatoFecha = SimpleDateFormat("dd/mm/yyyy")
+
+                val estudianteId = usuario?.id
+                val tituloIngresado = titulo.text.toString()
+                val detalleIngresado = descripcion.text.toString()
+                val creditosIngresados = creditos.text.toString().toInt()
+                val semestreSeleccionado = semestre.selectedItem.toString().toInt()
+                val fechaString = fechaText.text.toString()
+                val fecha = formatoFecha.parse(fechaString)
+                val fechaTimestamp = fecha.time
+                val fechaIngresada = fechaTimestamp
+
+
+                val reclamo = ReclamoDTOMobile(
+                    true,
+                    creditosIngresados,
+                    detalleIngresado,
+                    estudianteId!!,
+                    1,
+                    fechaIngresada,
+                    null,
+                    semestreSeleccionado,
+                    tituloIngresado
+                )
+
+                Log.d("AgregarReclamoActivity", "eventoId: ${eventoId}")
+
+                // Convertir la cadena de fecha a un objeto Date
+                //    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                // val date: Date = dateFormat.parse(fechaIngresada.toString()) ?: Date()
+
+
+                val call = apiService.agregarReclamo(
+                    reclamo
+                )
+
+                call.enqueue(object : Callback<ReclamoDTOMobile> {
+                    override fun onResponse(
+                        call: Call<ReclamoDTOMobile>,
+                        response: Response<ReclamoDTOMobile>
+                    ) {
+                        if (response.isSuccessful) {
+                            val reclamoResp = response.body()
+                            Log.d("AgregarReclamoActivity", "ResponseBody: $reclamoResp")
+                            mostrarDialogoReclamoCreado()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ReclamoDTOMobile>, t: Throwable) {
+                        Log.d("AgregarReclamoActivity", "error: ${t}")
+                    }
+                })
+
+            }
+        }
+
         volver.setOnClickListener{
             // val mainActivity = Intent(this@AgregarReclamoActivity, MainActivity::class.java)
             //  startActivity(mainActivity)
@@ -121,5 +265,16 @@ class AgregarReclamoVMEActivity : AppCompatActivity() {
         datePickerDialog.show()
     }
 
+    private fun mostrarDialogoReclamoCreado() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Reclamo creado")
+        builder.setMessage("Su reclamo ha sido creado correctamente.")
+        builder.setPositiveButton("Aceptar") { dialog, which ->
+            // Al hacer clic en Aceptar, cierra la Activity
+            finish()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
 
 }
