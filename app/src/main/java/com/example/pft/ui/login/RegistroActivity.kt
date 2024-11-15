e489abddeb998ecb84c91906e018f2a433392a9b
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2024-11-15 17:26:44 -0300

    Quito comentario en registro

--- app/src/main/java/com/example/pft/ui/login/RegistroActivity.kt
+++ app/src/main/java/com/example/pft/ui/login/RegistroActivity.kt
@@ -133,12 +133,7 @@ class RegistroActivity : AppCompatActivity() {
         mensaje_tipoUsuario = findViewById(R.id.registro_mensaje_tipo)
         registro_mensaje = findViewById(R.id.registro_Mensaje)
 
-        /*val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
-        val apiService = retrofit.create(ApiService::class.java)*/
         val apiService = ApiClient.getApiService(this)
         //-------------Spinner ITR---------------------------------------------------------
 
