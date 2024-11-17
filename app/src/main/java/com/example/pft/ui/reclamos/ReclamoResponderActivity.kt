93afba939a83bd9227fbdcdc9bb0de31323fb479
emanuellvz <116662539+EmanuelLap@users.noreply.github.com>
2024-11-17 20:37:54 -0300

    Revert "Actualizo clases Reclamo, ReclamoResponder y layout reclamo"
    This reverts commit 09a99f363ef7850f2952c26a0928518f2dd879ee.

--- app/src/main/java/com/example/pft/ui/reclamos/ReclamoResponderActivity.kt
+++ app/src/main/java/com/example/pft/ui/reclamos/ReclamoResponderActivity.kt
@@ -2,25 +2,14 @@ package com.example.pft.ui.reclamos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import com.example.pft.R
 import com.example.pft.entidades.ReclamoDTO
 import com.google.gson.Gson
-
-private lateinit var respuesta: EditText
-private lateinit var btn_responder: Button
-
 
 class ReclamoResponderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reclamo_responder)
-
-        respuesta=findViewById(R.id.reclamo_respuesta)
-        btn_responder=findViewById(R.id.reclamo_btnResponder)
-
-
 
         val reclamoJson = intent.getStringExtra("reclamo")
 
